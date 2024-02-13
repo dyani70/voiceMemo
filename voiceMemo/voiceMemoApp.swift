5c27280d47dd92d1ac35851a52dc940cc480669e
Dyani <dyani70@gmail.com>
2024-02-14 00:01:08 +0900

    Initial Commit

---
+++ voiceMemo/voiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiceMemoApp.swift
+//  voiceMemo
+//
+//  Created by Dyani on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct voiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
